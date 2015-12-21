0a5534986f9e74bda6a88181902ac3bd756a7ced
Philip Dai <smartielion@gmail.com>
2015-12-21 08:07:25 -0500

    Adjustment

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 import angular from 'angular';
 import home from './components/home/home';
 
+console.log(home);
+console.log(home.name);
+
 let myApp = angular.module('myapp', [home.name]);
 
 export default myApp;
